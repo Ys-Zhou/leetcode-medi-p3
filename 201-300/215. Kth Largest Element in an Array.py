dd7d2a7bb64d9d42fab5668bd12cdb7dca4c338d
Ys-Zhou <zhouyan1993ys@gmail.com>
2018-12-19 15:06:18 +0900

    Y. Zhou

---
+++ 201-300/215. Kth Largest Element in an Array.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+
+        def half_quick_sort(start, end):
+            if start == end:
+                return nums[start]
+            left, right = start, end
+            while left < right:
+                if nums[left] < nums[end]:
+                    left += 1
+                    continue
+                if nums[right] >= nums[end]:
+                    right -= 1
+                    continue
+                nums[left], nums[right] = nums[right], nums[left]
+            nums[left], nums[end] = nums[end], nums[left]
+            if left == len(nums) - k:
+                return nums[left]
+            if left < len(nums) - k:
+                return half_quick_sort(left + 1, end)
+            if left > len(nums) - k:
+                return half_quick_sort(start, left - 1)
+
+        return half_quick_sort(0, len(nums) - 1)
