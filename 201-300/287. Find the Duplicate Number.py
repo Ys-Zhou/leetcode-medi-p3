5c71f2439a36565e15ef416ee6edbae96924b45b
yzhou <yzhou@yahoo-corp.jp>
2019-06-13 11:03:08 +0900

    y. zhou

--- 201-300/287. Find the Duplicate Number.py
+++ 201-300/287. Find the Duplicate Number.py
@@ -1,3 +1,18 @@
+# Runtime: 48 ms, faster than 43.36% of Python3 online submissions
+# Memory Usage: 14.5 MB, less than 84.65% of Python3 online submissions
+
+
 class Solution:
     def findDuplicate(self, nums: list) -> int:
-        pass
+        fast = nums[0]
+        slow = nums[0]
+        while True:
+            fast = nums[nums[fast]]
+            slow = nums[slow]
+            if fast == slow:
+                break
+        fast = nums[0]
+        while fast != slow:
+            fast = nums[fast]
+            slow = nums[slow]
+        return fast
